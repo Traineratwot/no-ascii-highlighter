e4f9727b4735c46ecd9eb4d98bd4af2c88c441bd
GitHub Action <action@github.com>
2022-10-26 09:08:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/traineratwot/noasciihighlighter/MyBundle.kt
+++ src/main/kotlin/com/github/traineratwot/noasciihighlighter/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.traineratwot.noasciihighlighter
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/traineratwot/noasciihighlighter/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/traineratwot/noasciihighlighter/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.traineratwot.noasciihighlighter.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.traineratwot.noasciihighlighter.MyBundle
 
 class MyApplicationService {
 
